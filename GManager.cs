826c31258f3c297a8f266ed51c08bd97f4e8f13a
Bill Shackleton <62378735+BillShackleton@users.noreply.github.com>
2021-12-15 19:06:22 +0000

    Add files via upload

---
+++ GManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GManager : MonoBehaviour
+{
+    public Text speedText;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject Kart = GameObject.Find("Kart");
+        KartController controller = Kart.GetComponent<KartController>();
+        speed = controller.currentSpeed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject Kart = GameObject.Find("Kart");
+        KartController controller = Kart.GetComponent<KartController>();
+        speed = controller.currentSpeed;
+
+        speedText.text = speed.ToString("F0");
+    }
+
+}
